0505eb0da9fe50da8cb93976d9cfb964e568c0ad
Dr. Dillinger <peter.dillinger@srh.de>
2023-10-27 11:57:28 +0200

    Jetzt mit Endlosschleife, mit Abbruch bei Eingabe einer 0.

--- src/BrücheKürzen.java
+++ src/BrücheKürzen.java
@@ -3,29 +3,32 @@ import java.util.Scanner;
 public class BrücheKürzen {
     public static void main(String[] args) {
         Scanner eingabe = new Scanner( System.in );
-        System.out.print("Zähler: ");
-        int zähler = eingabe.nextInt();
-        System.out.print("Nenner: ");
-        int nenner = eingabe.nextInt();
 
-        System.out.println("Der Bruch lautet: " + zähler + "/" + nenner);
+        while (true) {
+            System.out.print("Zähler: ");
+            int zähler = eingabe.nextInt();
+            System.out.print("Nenner: ");
+            int nenner = eingabe.nextInt();
+            if (zähler == 0 || nenner == 0) break;
 
-        // 1) ggT finden (gemeinser Teiler der beiden Zahlen Zähler und Nenner = Euklid)
-        int a = zähler;
-        int b = nenner;
-        while (a != b) {
-            if (a > b) {
-                a = a - b;
+            System.out.println("Der Bruch lautet: " + zähler + "/" + nenner);
+
+            // 1) ggT finden (gemeinser Teiler der beiden Zahlen Zähler und Nenner = Euklid)
+            int a = zähler;
+            int b = nenner;
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                } else {
+                    b = b - a;
+                }
             }
-            else {
-                b = b - a;
-            }
+            int teiler = a;
+
+            // 2) Zähler und Nenner durch den ggT teilen
+            zähler = zähler / teiler;
+            nenner = nenner / teiler;
+            System.out.println("Der gekürzte Bruch lautet: " + zähler + "/" + nenner);
         }
-        int teiler = a;
-
-        // 2) Zähler und Nenner durch den ggT teilen
-        zähler = zähler / teiler;
-        nenner = nenner / teiler;
-        System.out.println("Der gekürzte Bruch lautet: " + zähler + "/" + nenner);
     }
 }
